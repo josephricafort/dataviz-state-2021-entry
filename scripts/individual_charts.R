2925717b63d1a511239f75085b42b00af116b2fc
josephricafort <josephricafortjr@gmail.com>
2022-07-10 15:41:56 +0800

    Generate charts for improved explainer page

--- scripts/individual_charts.R
+++ scripts/individual_charts.R
@@ -79,6 +79,27 @@ for(row_n in 1:nrow(tribe_tally)){
 
 ###- All personas in single chart -###
 
+# Percent distribution all 16 personalities #
+ggplot(tribe_tally %>% 
+         mutate(group = paste(experience, roleType, incomeGroup, commitment)) %>%
+         mutate(group = fct_reorder(group, n)) %>%
+         mutate(class = "class"), aes(reorder(group, nPerc), nPerc, fill = class)) + 
+  geom_col(show.legend = F) +
+  coord_flip() +
+  labs(
+    x = "Personas",
+    y = "Percent of the Respondents"
+  )
+
+ggsave(
+  "chartdistribution-all.svg",
+  last_plot(),
+  "svg",
+  "plots",
+  width = 12,
+  height = 12
+)
+
 # Tribe tools #
 ggplot(tribe_tools %>% 
          filter(tools_for_dv_vars %in% tools_top10), 
@@ -120,4 +141,128 @@ ggsave(
   "plots/chartindustry",
   width = 18,
   height = 12
+)
+
+# Tribe industries but TILEMAP #
+orgsec_ordered <- rev(c("Academia",
+                        "Marketing",
+                        "Finance",
+                        "Healthcare/medical",
+                        "Information technology",
+                        "Journalism",
+                        "Public sector (government)",
+                        "Private sector",
+                        "Non-profit",
+                        "Consultant (therefore multiple areas)"))
+
+ggplot(tribe_orgsec %>%
+         mutate(org_sector_names = fct_relevel(org_sector_names,
+                                               orgsec_ordered)), 
+       aes(
+  org_sector_names, 
+  reorder(tribe_desc, desc(tribe_desc)), 
+  fill = n)) +
+  geom_tile(color = "white",
+            lwd = 0.5) +
+  scale_fill_gradient2(low = "#FFFFFF", 
+                       high = "#335E35",
+                       mid = "#66BB6A",
+                       midpoint = max(tribe_orgsec$n)/2,
+                       na.value = "#FFFFFF") +
+  coord_fixed() +
+  scale_x_discrete(position = "top", limits=rev) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 0, hjust = 0)) +
+  guides(fill = guide_colorbar(barwidth = 0.5,
+                               barheight = 20))
+
+ggsave(
+  "chartindustry-tilemap.svg",
+  last_plot(),
+  "svg",
+  "plots/chartindustry",
+  width = 12,
+  height = 8
+)
+
+
+
+### Factors charts ###
+
+# Years experience #
+ggplot(experience_sorted, aes(YearsDVExperience, nPerc, fill = experience)) + 
+  geom_col() +
+  coord_flip() +
+  labs(
+    x = "Years of Experience",
+    y = "Percent of the Respondents",
+    fill = "Experience"
+  )
+
+ggsave(
+  "yearsexperience.svg",
+  last_plot(),
+  "svg",
+  "plots/indicators",
+  width = 9,
+  height = 6
+)
+
+# Role #
+ggplot(role_tally %>% 
+         mutate(Class = "class"), 
+       aes(Class, nPerc, fill = RoleType)) +
+  geom_bar(position = "stack", stat = "identity") +
+  coord_flip() + 
+  labs(
+    x = "",
+    y = "Percent of the Respondents",
+    fill = "Role Type"
+  )
+
+ggsave(
+  "role.svg",
+  last_plot(),
+  "svg",
+  "plots/indicators",
+  width = 9,
+  height = 1
+)
+
+# Income #
+ggplot(income_sorted, aes(PayAnnual, nPerc, fill = income_group)) +
+  geom_col() +
+  coord_flip() +
+  labs(
+    x = "Annual Pay",
+    y = "Percent of the Respondents",
+    fill = "Income Group"
+  )
+
+ggsave(
+  "income.svg",
+  last_plot(),
+  "svg",
+  "plots/indicators",
+  width = 9,
+  height = 6
+)
+
+# Commitment #
+ggplot(commitment_tally %>%
+         mutate(Class = "class"), 
+       aes(Class, nPerc, fill = Commitment)) +
+  geom_bar(position = "stack", stat = "identity") + 
+  coord_flip() +
+  labs(
+    x = "",
+    y = "Percent of the Respondents"
+  )
+
+ggsave(
+  "commitment.svg",
+  last_plot(),
+  "svg",
+  "plots/indicators",
+  width = 9,
+  height = 1
 )
